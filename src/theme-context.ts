6ed421bf023cf458b4149a7267beb5eb06a95b18
AAGaming <aa@mail.catvibers.me>
2023-02-04 17:13:47 -0500

    fix relativeURL

--- src/theme-context.ts
+++ src/theme-context.ts
@@ -26,8 +26,8 @@ export class WikiJSThemeRenderContext extends MarkdownThemeRenderContext {
     }
   }
 
-  getRelativeUrl(url: string | undefined): string | null {
-    const ret = super.getRelativeUrl(url)?.replace(".md", "")?.replaceAll("_internal_", "INTERNAL")?.replaceAll("_", "/")?.replaceAll("_internal_", "INTERNAL") || null;
+  relativeURL = (url: string | undefined): string | null => {
+    const ret = super.relativeURL(url)?.replace(".md", "")?.replaceAll("_internal_", "INTERNAL")?.replaceAll("_", "/")?.replaceAll("_internal_", "INTERNAL") || null;
     return ret;
   }
 }
